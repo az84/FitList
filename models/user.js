ed5b41fc5039ed7f08143aee287984a59fc6e19d
azaidi <zaidialina1@gmail.com>
2021-09-12 12:54:57 -0400

    signup handlebar,signup.js and post route

--- models/user.js
+++ models/user.js
@@ -16,11 +16,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,

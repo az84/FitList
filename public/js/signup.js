a09487c41384e65c0d5be0a850ff51c03f5f6002
DS <derekstander@gmail.com>
2021-09-15 17:19:53 -0400

    modified:   controllers/homeRoutes.js renamed test to create workout. needs work 	modified:   models/index.js changed relationship from through: WorkoutExercise, to through: model:WorkoutExercise, i think this is better. i am not sure. 	modified:   public/js/login.js login redirects to profile page 	modified:   public/js/signup.js signup redirects to profile page 	modified:   views/homepage.handlebars not working just testing. 	new file:   views/partials/exerciseinfo.handlebars was attempting to make a partail but i think i got it working on profile. 	modified:   views/profile.handlebars displays the users workout(s) and the exercises that go with them only tested with 1 workout having 2 exercises the data is there, it just looks like trash atm.

--- public/js/signup.js
+++ public/js/signup.js
@@ -13,7 +13,7 @@ const signupFormHandler = async function(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to sign up');
     }

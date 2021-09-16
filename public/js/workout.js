272a7fbb930008372b0d8c0bbfe61ee5ee99f750
DS <derekstander@gmail.com>
2021-09-16 19:20:56 -0400

    modified:   controllers/homeRoutes.js fixed loggedin for create workout page 	modified:   public/js/workout.js redirects to profile 	modified:   views/createworkout.handlebars default values set to 0 for now

--- public/js/workout.js
+++ public/js/workout.js
@@ -1,21 +1,23 @@
 const newWorkout = async () => {
   event.preventDefault();
   
-  const workoutName = document.querySelector('#workout_name').value.trim();
-  const name = document.querySelector('#excercise').value.trim(); //exercise name
-  const category = document.querySelector('#category').value.trim();
-  const equipment = document.querySelector('#equipment').value.trim();
-  const type = document.querySelector('#type').value.trim();
-  const muscle = document.querySelector('#muscle').value.trim();
-  const reps = document.querySelector('#reps').value.trim();
-  const sets = document.querySelector('#sets').value.trim();
-  const distance = document.querySelector('#distance').value.trim();
-  const duration = document.querySelector('#duration').value.trim();
-  const weight = document.querySelector('#weight').value.trim();
+  const workoutName = document.querySelector("#workout_name").value.trim();
+  const name = document.querySelector("#excercise").value.trim(); //exercise name
+  const category = document.querySelector("#category").value.trim();
+  const equipment = document.querySelector("#equipment").value.trim();
+  const type = document.querySelector("#type").value.trim();
+  const muscle = document.querySelector("#muscle").value.trim();
+  const reps = document.querySelector("#reps").value.trim();
+  const sets = document.querySelector("#sets").value.trim();
+  const distance = document.querySelector("#distance").value.trim();
+  const duration = document.querySelector("#duration").value.trim();
+  const weight = document.querySelector("#weight").value.trim();
+
+  
 
   const response = await fetch('/api/workouts/', {
     method: 'POST',
-    body: JSON.stringify({ workoutName, name, equipment, type, muscle, sets, reps, weight,
+    body: JSON.stringify({ workoutName, category, name, equipment, type, muscle, sets, reps, weight,
       distance, duration, sets, reps } ),
     headers: {
       'Content-Type': 'application/json'
@@ -23,7 +25,7 @@ const newWorkout = async () => {
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace('/profile');
   } else {
     alert(response.statusText);
   }

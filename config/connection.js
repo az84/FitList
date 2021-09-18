3afb99aa8f9054dce24444056d959cd5bc53130c
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-18 04:55:38 -0400

    jawsfix?

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -16,5 +14,3 @@ if (process.env.JAWSDB_URL) {
     },
   );
 }
-
-module.exports = sequelize;

f9b7467c3b0c7b46ecf6b1eb44a575a51f7b2f8a
DS <derekstander@gmail.com>
2021-09-18 13:30:18 -0400

    modified:   models/index.js capitalization 	modified:   public/js/workout.js looks like a fetch got pulled back in during some merging. this caused 2 fetch's to run. and 2 workouts, 1 with 1 E. and 1 with 2E's to be made. one was a dupe. this was removed so workflow was add then create

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Workout = require('./Workout');
-const Exercise = require('./Exercise');
+const User = require('./user');
+const Workout = require('./workout');
+const Exercise = require('./exercise');
 
 const WorkoutExercise = require('./WorkoutExercise');
 

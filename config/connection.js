6b4b996785fc3405cf6cf76d794c92a9c0875f5d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-09-18 09:52:25 -0400

    heroku trouble shooting

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ let sequelize = require('sequelize');
 require('dotenv').config();
 
 if (process.env.JAWSDB_URL) {
-  Sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new sequelize(process.env.JAWSDB_URL);
 } else {
-  Sequelize = new Sequelize(
+  sequelize = new sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,

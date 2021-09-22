9a4d58b43b77c2e84a2f609a1f3487beff2e3e81
DS <derekstander@gmail.com>
2021-09-22 15:58:35 -0400

    modified:   public/js/createworkout.js value as number wasnt worked swaped back to value.trim 	modified:   public/js/css/style.css card width lowered 	modified:   views/exercise.handlebars tried to make the cards look better

--- public/js/createworkout.js
+++ public/js/createworkout.js
@@ -1,20 +1,19 @@
 const newWorkout = async (event) => {
   event.preventDefault();
 
-
 //const eList = document.querySelectorAll('#exerciseForm.input');
 let workoutname = document.getElementById('workout_name').value.trim();
-let date = document.getElementById('date').value;
+let date = document.getElementById('date').value.trim();
 let category = document.getElementById('category').value.trim();
 let name = document.getElementById('excercise').value.trim(); //exercise name
 let equipment = document.getElementById('equipment').value.trim();
 let type = document.getElementById('type').value.trim();
 let muscle = document.getElementById('muscle').value.trim();
-let reps = document.getElementById('reps').valueAsNumber;
-let sets = document.getElementById('sets').valueAsNumber;
-let distance = document.getElementById('distance').valueAsNumber;
-let duration = document.getElementById('duration').valueAsNumber;
-let weight = document.getElementById('weight').valueAsNumber;
+let reps = document.getElementById('reps').value.trim();
+let sets = document.getElementById('sets').value.trim();
+let distance = document.getElementById('distance').value.trim();
+let duration = document.getElementById('duration').value.trim();
+let weight = document.getElementById('weight').value.trim();
 
 let allE = document.querySelectorAll('.e'); 
 let ex = allE.length;
@@ -28,7 +27,7 @@ case 10:
   var categoryone = document.getElementById('anothercategory').textContent
   var typeone = document.getElementById('anothertype').textContent;
   var muscleone = document.getElementById('anothermuscle').textContent;
-  var setsone = document.getElementById('moresets').valueAsNumber;
+  var setsone = document.getElementById('moresets').textContent;
   var repsone = document.getElementById('morereps').textContent;
   var weightone = document.getElementById('moreweight').textContent;
   var distanceone = document.getElementById('moredistance').textContent;
